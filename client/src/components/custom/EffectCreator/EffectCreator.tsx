c6284e12badd4006b9b148b2d63f4511dc4111cb
NorbertSoos <nso@frankly.dk>
2024-01-22 18:10:09 +0100

    update

--- client/src/components/custom/EffectCreator/EffectCreator.tsx
+++ client/src/components/custom/EffectCreator/EffectCreator.tsx
@@ -1,10 +1,14 @@
 'use client';
 import { useDispatch } from 'react-redux';
 import { setEffect } from '@/state/features/effect/effectSlice';
+import { Panel, PanelGroup } from 'react-resizable-panels';
+import { EffectStateT } from '@/types/effect.types';
 import EffectDetails from './EffectDetails/EffectDetails';
 import EffectPlayer from './EffectPlayer/EffectPlayer';
 import FrameGrid from './FrameGrid/FrameGrid';
-import { EffectStateT } from '@/types/effect.types';
+import ResizeHandle from '@/components/derived/ResizeHandle/ResizeHandle';
+import styles from './EffectCreator.module.scss';
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 const EffectCreator = ({ initialEffect }: { initialEffect: EffectStateT }) => {
 	const dispatch = useDispatch();
@@ -12,9 +16,17 @@ const EffectCreator = ({ initialEffect }: { initialEffect: EffectStateT }) => {
 
 	return (
 		<div className='flex gap-10'>
-			<div>
-				<FrameGrid />{' '}
-			</div>
+			<PanelGroup direction='horizontal'>
+				<Panel defaultSize={50}>
+					<div className={styles.empty}>empty</div>
+				</Panel>
+				<ResizeHandle />
+				<Panel defaultSize={50} minSize={40}>
+					<div>
+						<FrameGrid />{' '}
+					</div>
+				</Panel>
+			</PanelGroup>
 			<div className='flex flex-col gap-2'>
 				<EffectPlayer />
 				<EffectDetails />
--- client/src/components/derived/ResizeHandle/ResizeHandle.tsx
+++ client/src/components/derived/ResizeHandle/ResizeHandle.tsx
@@ -4,13 +4,15 @@ import UIIcon from '@/components/base/UIIcon/UIIcon';
 import styles from './ResizeHandle.module.scss';
 import appendClasses from '@/misc/hooks/appendClasses/appendClasses';
 
-type ResizeHandleProps = {
+const ResizeHandle = ({
+	id,
+	align,
+	className,
+}: {
 	id?: string;
 	className?: string;
 	align?: 'horizontal' | 'vertical';
-};
-
-const ResizeHandle = ({ id, align, className }: ResizeHandleProps) => {
+}) => {
 	return (
 		<PanelResizeHandle className={appendClasses([styles.ResizeHandleOuter, className])} id={id}>
 			<UIIcon name={Icons.resize} isRotated={align === 'vertical'} />
